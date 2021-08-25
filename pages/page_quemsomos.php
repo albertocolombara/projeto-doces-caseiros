7a4bdb3054cd2b20be424093a33cf71ed636bb4e
Alberto Colombara <albertocolombara@hotmail.com>
2021-08-25 02:14:04 -0300

    home e quem somos

---
+++ pages/page_quemsomos.php
@@ -0,0 +1,11 @@
+    <section class="quemsomos">
+        <div class="quemsomos_left">
+            <img src="images/img2.png" alt="Cocada">
+        </div>
+        <div class="quemsomos_right">
+            <h3>Quem somos</h3>
+            <p>Somos uma empresa ainda pequena, porém com muita força de vontade e com grandes sonhos, sonhos de se tornar uma referência no ramo da cocada.</p>
+            <br>
+            <p>Nossas deliciosas cocadas são desenvolvidas através de muito amor, dedicação e carinho. Elaboramos, criamos, projetamos e desenvolvemos nossos produtos de forma que nossos clientes sintam esse carinho em cada detalhe.</p>
+        </div>
+    </section>
