07109ee6c5f4b15932e345fb7dbd45fcfe49a862
Alberto Colombara <albertocolombara@hotmail.com>
2021-08-26 19:19:25 -0300

    função alterarTitulo

--- function/function.php
+++ function/function.php
@@ -7,4 +7,55 @@ function carregarPagina (){
         require_once('pages/page_home.php');
     endif;
 }
+
+function alterarTitulo (){
+    (isset($_GET['p'])) ? $pagina = $_GET['p'] : $pagina = 'home';
+    switch ($pagina):
+        case 'home':
+            $titulo = "Cocadinhas - A melhor cocada da região.";
+            break;
+
+        case 'quemsomos':
+            $titulo = "Quem somos - Cocadinhas";
+            break;
+              
+        case 'produtos':
+            $titulo = "Produtos - Cocadinhas";
+            break;  
+
+        case 'contato':
+            $titulo = "Contato - Cocadinhas";
+            break;  
+
+        case 'contato':
+            $titulo = "Contato - Cocadinhas";
+            break;    
+
+        case 'abacaxi':
+            $titulo = "Cocada de Abacaxí - Cocadinhas";
+            break; 
+
+        case 'chocolate':
+            $titulo = "Cocada de Chocolate - Cocadinhas";
+            break;
+
+        case 'maracuja':
+            $titulo = "Cocada de Maracujá - Cocadinhas";
+            break;    
+
+        case 'docedeleite':
+            $titulo = "Cocada de Doce de Leite - Cocadinhas";
+            break; 
+
+        case 'goiaba':
+            $titulo = "Cocada de Goiaba - Cocadinhas";
+            break;    
+
+        default:
+            $titulo = "Cocadinhas - A melhor cocada da região.";
+            break; 
+
+        endswitch;
+        return $titulo;
+}
 ?>
--- pages/header.php
+++ pages/header.php
@@ -2,7 +2,7 @@
 <html lang="pt-br">
 <head>
     <meta charset="utf-8">
-    <title>cocadinha</title>
+    <title><?php echo alterarTitulo() ?></title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
     <link rel="stylesheet" type="text/css" href="../../20048654-5_AlbertoColombara/css/style.css">
 </head>
